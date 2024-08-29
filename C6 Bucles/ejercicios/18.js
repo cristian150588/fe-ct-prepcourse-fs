bbf4781c02c52995afb3dd64015905fe38c72c57
CERomagnano <cristian_emmanuel_roma@hotmail.com>
2024-08-29 13:34:31 -0300

    ejercicios bucles

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,23 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+  let producto = 1;
+
+  if (a > b) {
+    let temp = a;
+    a = b;
+    b = temp;
+  }
+  let num = a;
+
+  while (num <= b) {
+    producto *= num;
+    num++;           
+  }
+  if (producto === -0) {
+    producto = 0;
+  }
+  return producto;
 }
 
 module.exports = productoEntreNúmeros;
